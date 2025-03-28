241d74fe31a3c8b089eede3ca7ab53f37bb58e22
adrianfa-f <adrianfrancoavila1995@gmail.com>
2025-03-28 19:13:45 -0400

    elimine .netlify de las url

--- src/pages/Dashboard/Header/ProfileMenu.jsx
+++ src/pages/Dashboard/Header/ProfileMenu.jsx
@@ -60,7 +60,7 @@ const ProfileMenu = () => {
       formData.append("file", file);
   
       const response = await fetch(
-        `/.netlify/functions/blobs/upload?userId=${currentUser.uid}&type=profile`, // Usar ruta absoluta
+        `/functions/blobs/upload?userId=${currentUser.uid}&type=profile`, // Usar ruta absoluta
         {
           method: "POST",
           body: formData,
@@ -99,7 +99,7 @@ const ProfileMenu = () => {
           {userData?.photoURL ? (
             <img 
               src={userData.photoURL.startsWith("blob:") 
-              ? `/.netlify/functions/blobs/get?userId=${currentUser.uid}&type=profile&blobId=${userData.photoURL.split(":")[3]}`
+              ? `/functions/blobs/get?userId=${currentUser.uid}&type=profile&blobId=${userData.photoURL.split(":")[3]}`
               : userData.photoURL
             }
               className="h-16 w-16 rounded-full object-cover cursor-pointer"
