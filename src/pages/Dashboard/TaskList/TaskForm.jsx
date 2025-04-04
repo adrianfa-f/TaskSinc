316a9583481bdfc824a260e77560e3bf33960c93
adrianfa-f <adrianfrancoavila1995@gmail.com>
2025-04-04 09:27:38 -0400

    se corrigio algunos estilos

--- src/pages/Dashboard/TaskList/TaskForm.jsx
+++ src/pages/Dashboard/TaskList/TaskForm.jsx
@@ -160,7 +160,7 @@ const TaskForm = ({ onClose }) => {
 
     return (
         <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 backdrop-blur-sm">
-            <div ref={formRef} className="bg-gray-50 rounded-xl p-6 w-full max-w-md mx-4 shadow-xl border border-gray-100 max-h-[90vh]">
+            <div ref={formRef} className="bg-gray-50 rounded-xl p-6 w-full max-w-md mx-4 shadow-xl border border-gray-100 max-h-[90vh] overflow-hidden">
                 <div className="flex justify-between items-center mb-6">
                     <h2 className="text-2xl font-bold text-gray-800"> Nueva Tarea </h2>
                     <button 
